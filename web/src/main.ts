cb4f16b0d8799be6e3da2ccb9c3fdd6d931dc98a
violedo <liumianzhi@qq.com>
2020-11-16 15:16:27 +0800

    an empty program

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { Vue } from 'vue-class-component';
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 createApp(App)
   .use(router)
