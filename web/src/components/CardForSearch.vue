65e99c95d4606649908da496f7177adfa9ee4d56
AkangAkang <975376945@qq.com>
2020-11-17 18:21:08 +0800

    feat: finish search and card

--- web/src/components/CardForSearch.vue
+++ web/src/components/CardForSearch.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="cardForSearch">
-    <a-card hoverable title="金平路" size="small" style="border-radius : 3px">
-     
+    <a-card hoverable :title="info.title" size="small" style="border-radius :5px">
+      <a-descriptions title="关键字" size="small">
+        <a-descriptions-item v-for="(item) in info.keyWords" v-bind:key="item.index">{{item}}</a-descriptions-item>
+      </a-descriptions>
+      <a-divider />
+      <a-descriptions title="属性" size="small">
+        <a-descriptions-item v-for="(item) in info.attributes" v-bind:key="item.index">
+          <a-tag id="tagBack" color="#eeeeee">
+            {{item[0]}}
+            <!-- <span style="text-align:right">
+{{item[1]}}
+            </span> -->
+            <a-tag color="#a3dbd8" style="align:right,margin-left ：100px">{{item[1]}}</a-tag>
+          </a-tag>
+        </a-descriptions-item>
+      </a-descriptions>
+      <a-divider />
+      <a-descriptions title="标签" size="small">
+        <a-descriptions-item v-for="(item) in info.tags" v-bind:key="item.index">
+          <a-tag color="#a3dbd8">
+            {{item}}
+
+          </a-tag>
+        </a-descriptions-item>
+      </a-descriptions>
     </a-card>
-    <br/>
+    <br />
   </div>
 </template>
        
@@ -12,12 +35,13 @@
 <script >
 import moment from "moment";
 export default {
+  props: ["info"],
   data() {
     return {
       likes: 0,
       dislikes: 0,
       action: null,
-    
+
       moment
     };
   },
@@ -35,3 +59,33 @@ export default {
   }
 };
 </script>
+<style >
+.ant-card-head-title {
+  display: inline-block;
+  flex: 1;
+  padding: 16px 0;
+  overflow: hidden;
+  white-space: nowrap;
+  font-weight: bold;
+  text-overflow: ellipsis;
+  font-size: 16px;
+}
+
+.ant-descriptions-title {
+  flex: auto;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.85);
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 1.2;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+#tagBack{
+   width: 100px;
+}
+/* .ant-tag-has-color, .ant-tag-has-color a, .ant-tag-has-color a:hover, .ant-tag-has-color .anticon-close, .ant-tag-has-color .anticon-close:hover {
+    width: 70px;
+} */
+</style>
--- web/src/views/Search.vue
+++ web/src/views/Search.vue
@@ -5,7 +5,8 @@
         <a-row>
           <a-col :span="20">
             <a-input-search
-              placeholder="input search text"
+              placeholder="输入您的问题"
+              @search="onSearch"
               style="{'box-shadow': 5px 5px 10px gray}"
             />
           </a-col>
@@ -16,18 +17,14 @@
         <div>
           <a-tabs default-active-key="1" @change="callback" :size="small">
             <a-tab-pane key="1" tab="问题">
-
-              <QuestionForSearch v-for="(item) in data" v-bind:key="item.id" :ques="item"/>
-
+              <QuestionForSearch v-for="(item) in questionData" v-bind:key="item.id" :ques="item" />
             </a-tab-pane>
             <a-tab-pane key="2" tab="用户">用户</a-tab-pane>
           </a-tabs>
         </div>
       </a-col>
       <a-col :span="8" :offset="1">
-<CardForSearch/>
-
-
+        <CardForSearch :info="cardInfo"/>
       </a-col>
     </a-row>
   </div>
@@ -37,73 +34,122 @@
 import { Options, Vue } from "vue-class-component";
 import QuestionForSearch from "@/components/QuestionForSearch.vue";
 import CardForSearch from "@/components/CardForSearch.vue";
+import server from "@/http/request.js";
+
 const data = [
   {
     id: 1,
     title: "Ant Design Title 1",
-    user:"akvfcdg",
-    description:"dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
-    likeNum :123,
-    dislikeNum:4567,
-    commentNum:7890,
-
+    user: "akvfcdg",
+    description: "dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
+    likeNum: 123,
+    dislikeNum: 4567,
+    commentNum: 7890
   },
   {
     id: 2,
     title: "A7654 Title 1",
-    user:"dfsfskang",
-    description:"dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
-    likeNum :123,
-    dislikeNum:4567,
-    commentNum:7890,
+    user: "dfsfskang",
+    description: "dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
+    likeNum: 123,
+    dislikeNum: 4567,
+    commentNum: 7890
   },
   {
-     id: 3,
+    id: 3,
     title: "gfds 1",
-    user:"ererang",
-    description:"dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
-    likeNum :123,
-    dislikeNum:4567,
-    commentNum:7890,
+    user: "ererang",
+    description: "dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
+    likeNum: 123,
+    dislikeNum: 4567,
+    commentNum: 7890
   },
   {
-     id: 4,
+    id: 4,
     title: "ytre",
-    user:"akgfds",
-    description:"dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
-    likeNum :123,
-    dislikeNum:4567,
-    commentNum:7890,
+    user: "akgfds",
+    description: "dfghjklfcghjm,dfgbndghjkrewqwertyuiytr",
+    likeNum: 123,
+    dislikeNum: 4567,
+    commentNum: 7890
   }
 ];
 
-@Options({
-  components: {
-    QuestionForSearch,
-     CardForSearch
-  }
-})
-export default class Search extends Vue {
+const data2={
+  title:"上海交通大学",
+  keyWords:["国际知名大学","二月十三","C9高校"],
+  attributes:[["前身","南洋公学"],["校长","林忠钦"],["位于","上海"],["党委书记","姜斯宪"]],
+  tags:["二月十三","复旦"]
+
+}
+export default {
+  components: { QuestionForSearch, CardForSearch },
   data() {
     return {
-      data
+      questionData: data,
+      searchValue: "",
+      cardInfo:data2
     };
-  }
-}
-// export default {
-//   data() {
-//     return {
-//       data
-//     };
-//   }
+  },
+  methods: {
+    onSearch(value) {
+      this.searchValue = value;
+      console.log(this.searchValue);
 
-// };
-// export default class Search extends Vue {}
+      server
+        .post("/search", {
+          value: this.searchValue
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+        .then(response => {
+          this.questionData= response.data.questionData;
+        });
+    },
+
+    handleInit(response) {
+      this.questionData = response.data.questionData;
+      this.cardInfo=response.data.cardInfo;
+    }
+  },
+  created: function() {
+    server
+      .get("/get", {
+        params: {}
+      })
+      .then(response => this.handleInit(response))
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
 </script>
 
 <style>
 #search {
   min-height: 667px;
   background-color: #edeeed;
+}
+
+.ant-input-affix-wrapper {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-variant: tabular-nums;
+    list-style: none;
+    font-feature-settings: 'tnum';
+    position: relative;
+    display: inline-flex;
+    border: 1px solid #d9d9d9;
+    border-radius: 20px;
+    padding: 4px 11px;
+    width: 100%;
+    text-align: start;
+    background-color: #fff;
+    background-image: none;
+    color: rgba(0, 0, 0, 0.65);
+    font-size: 14px;
+    line-height: 1.5715;
 }
 </style>
