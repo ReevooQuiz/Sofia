95c6186b1923e216145d5efa50b5bfeece425010
zhanghanchong <308011618@qq.com>
2020-11-17 16:46:41 +0800

    feat: credentials.env

--- backend/qa-service/dao/QuestionsDaoImpl.go
+++ backend/qa-service/dao/QuestionsDaoImpl.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"github.com/zhanghanchong/qa-service/entity"
 	"gopkg.in/mgo.v2"
@@ -16,7 +17,8 @@ var mongoUrl string
 func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
-	mongoUrl = "test:test@localhost/mydb"
+	_ = godotenv.Load(os.Getenv("WORK_DIR") + "credentials.env")
+	mongoUrl = os.Getenv("MONGO_URL")
 }
 
 func (q *QuestionsDaoImpl) FindByQid(qid string) (question entity.Questions, err error) {
--- backend/qa-service/go.mod
+++ backend/qa-service/go.mod
@@ -3,6 +3,7 @@ module github.com/zhanghanchong/qa-service
 go 1.15
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/sirupsen/logrus v1.7.0
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
