9449bdf6172c7b0d7e4b34ba00ba1963e10ff724
zhanghanchong <308011618@qq.com>
2021-01-06 22:58:34 +0800

    feat: files for deployment

---
+++ redeploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker kill $(docker ps -aq)
+docker rmi -f $(docker images -q)
+docker system prune -f
+docker volume rm $(docker volume ls -qf dangling=true)
+docker network rm $(docker network ls -q)
+docker-compose up -d
